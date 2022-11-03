990471ffeee86513214e2e2471498f2b5b968702
saranya-sinseeda <79294193+saranya-sinseeda@users.noreply.github.com>
2022-11-03 23:53:16 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Preload from './pages/Preload';
+import Favorite from './pages/Favorite';
+import Profile from './pages/Profile';
+
+
+const Stack = createNativeStackNavigator();
+
+function PageStack() {
+  return (
+    <Stack.Navigator 
+    >
+      <Stack.Screen 
+        name="Preload" 
+        component={Preload} 
+        options={{headerShown:false}}
+      />
+      <Stack.Screen 
+        name="Favorite" 
+        component={Favorite} 
+        options={{headerShown:false}}
+      />
+      <Stack.Screen 
+        name="Profile" 
+        component={Profile} 
+        options={{headerShown:false}}
+      />
+     
+      
+    </Stack.Navigator>
+
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <PageStack/>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
